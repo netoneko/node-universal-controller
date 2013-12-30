c202c99319e9b6a9b00c4a187386def19069a00c
Kirill Maksimov <netoneko@gmail.com>
2013-12-30 23:45:34 +0200

    Rename index.js to app.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var app = require("./index");
+var app = require("./app");
 var http = require("http").createServer(app);
 var io = require("socket.io").listen(http);
 var redis = require("node-redis").createClient();
