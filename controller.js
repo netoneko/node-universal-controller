3ad89e1c60fd667fef10306d8f35ca5d90078485
Kirill Maksimov <netoneko@gmail.com>
2013-12-30 23:53:16 +0200

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,11 +1,11 @@
-var controller = function(handlers) {
-  this.subHandlers = handlers.reduce(function(memo, action) {
+var controller = function(actions) {
+  this.subscribeActions = actions.reduce(function(memo, action) {
     memo[action.sub] = action.handler;
     return memo;
   }, {});
 
   appendRoute = function(key, callback) {
-    handlers.map(function(action) {
+    actions.map(function(action) {
       if (action[key]) {
         callback(action);
       }
@@ -21,30 +21,32 @@ var controller = function(handlers) {
     });
   };
 
- this.websocket = function(socket) {
-   appendRoute("socket", function(action) {
-     socket.on(action.socket, function(data) {
-       var request = { "params": data };
-       var value = action.handler(request);
+  this.websocket = function(socket) {
+    appendRoute("socket", function(action) {
+      socket.on(action.socket, function(data) {
+        var request = { "params": data };
+        var value = action.handler(request);
 
-       socket.emit("result(" + action.socket +")", { "status": 200, "data": value });
-     });
-  });
- };
-
- this.redis = function(redis) {
-   redis.on("message", function (channel, message) {
-     try {
-       var request = { "params": JSON.parse(message.toString()) };
-       var value = subHandlers[channel](request);
-       console.log(value);
-     } catch(e) { }
+        socket.emit("result(" + action.socket +")", { "status": 200, "data": value });
+      });
    });
+  };
 
-   appendRoute("sub", function(action) {
-     redis.subscribe(action.sub);
-   });
- };
+  this.redis = function(redis) {
+    redis.on("message", function (channel, message) {
+      try {
+        var request = { "params": JSON.parse(message.toString()) };
+        var value = subscribeActions[channel](request);
+        console.log(value);
+      } catch(e) {
+        console.error(e.stack);
+      }
+    });
+
+    appendRoute("sub", function(action) {
+      redis.subscribe(action.sub);
+    });
+  };
 
   return this;
 };
